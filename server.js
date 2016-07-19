fbe50c099892cda6aa72f2482b1bdfaa2138e5db
Smitha Halappa Mallikarjunappa <hmsmitha17@gmail.com>
2016-07-19 12:23:25 -0700

    Added underscore library

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 var app = express();
 var PORT = process.env.PORT || 3000;
 
@@ -19,9 +20,13 @@ app.get('/todos', function(req, res){
 });
 
 app.post('/todos', function(req,res){
-	var body = req.body;
-    var id =  todonextId++;
 
+	var body = _.pick(req.body, 'description', 'completed');
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0){
+		return res.status(400).send();
+	}
+	body.description = body.description.trim();
+    var id =  todonextId++;
     body.id = id;
     todos.push(body);
     res.json(body);
@@ -31,15 +36,10 @@ app.post('/todos', function(req,res){
 app.get('/todos/:id', function(req,res){
 	//params.id will be string so convert to Integer. Second argument is base10
 	var todoid = parseInt(req.params.id, 10);
-	var matchFound;
-	todos.forEach(function(todoArray){
-               if(todoArray.id === todoid){
-               	 matchFound = todoArray;
-               }
-	});
+	var matchFound = _.findWhere(todos, {id: todoid});
 
   if(!matchFound){
-   res.status(400).send();
+   res.status(404).send();
   }else{
   	res.json(matchFound);
   }
